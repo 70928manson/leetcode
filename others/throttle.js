4e9e58f6d1b29963ccc12bd289e67c6230a32be7
Manson <manson972@yahoo.com>
2025-01-28 12:40:16 +0800

    modify: update throttle comment

--- others/throttle.js
+++ others/throttle.js
@@ -1,6 +1,6 @@
 // 節流 (throttle) 指的是，在一段時間（t）內只會執行一次觸發事件的回調 (callback) 函式，若在這之中又有新事件觸發，則不執行此回調函式。
 
-// 節流函式 throttle，此函式接收兩個參數，第一個參數是一個回調函式 fn，第二個參數是延遲時間 delay，此函式遵循以上描述的行為。
+// 節流函式 throttle，此函式接收兩個參數，第一個參數是一個回調函式 fn，第二個參數是延遲時間 delay。
 
 function throttle(fn, delay) {
     let timer;
