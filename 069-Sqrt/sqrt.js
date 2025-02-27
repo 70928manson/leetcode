ee1e7c8cb9d309165dff1ba2c296296356f0e958
70928manson <manson972@yahoo.com>
2025-02-27 09:42:16 +0800

    feat: add 069. Sqrt(x) solution

---
+++ 069-Sqrt/sqrt.js
@@ -0,0 +1,24 @@
+/**
+    Binary Search
+ */
+
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function(x) {
+    let left = 0;
+    let right = x;
+
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+
+        if (mid * mid <= x && (mid + 1) * (mid + 1) > x) {
+            return mid;
+        } else if (mid * mid > x) {
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        };
+    };
+};
