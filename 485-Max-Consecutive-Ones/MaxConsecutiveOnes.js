b99d0c4fcb6191c30c2d93eb45349fd7c29f4111
Manson <manson972@yahoo.com>
2025-03-02 13:19:40 +0800

    feat: add 485. Max Consecutive Ones solution

---
+++ 485-Max-Consecutive-Ones/MaxConsecutiveOnes.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMaxConsecutiveOnes = function (nums) {
+    let count = 0;
+
+    let temp = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === 1) {
+            temp++;
+            if (temp > count) {
+                count = temp;
+            };
+        } else {
+            temp = 0;
+        };
+    };
+
+    return count;
+};
