5454d5433db4c176b50bb7e24d3ae50e9f2bdaf7
Manson <manson972@yahoo.com>
2025-11-07 22:30:05 +0800

    feat: add 2125. Number of Laser Beams in a Bank solution

---
+++ 2125-Number-of-Laser-Beams-in-a-Bank/2125.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string[]} bank
+ * @return {number}
+ */
+var numberOfBeams = function (bank) {
+  // 先看當前行有幾個 ex: 3 個
+  // 看當前行的往下雷射有幾個 ex: 2 個
+  let count = [];
+  for (const devices of bank) {
+    let temp = 0;
+    for (device of devices) {
+      if (device === "1") {
+        temp++;
+      }
+    }
+
+    if (temp !== 0) count.push(temp);
+  }
+
+  let res = 0;
+
+  for (let i = 0; i < count.length - 1; i++) {
+    res += count[i] * count[i + 1];
+  }
+
+  return res;
+};
